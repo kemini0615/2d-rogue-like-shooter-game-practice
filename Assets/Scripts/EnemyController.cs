d97a7d7fc059edf7d59e6c6f36421cbd228562b7
kemini <moeday1010@gmail.com>
2025-04-13 11:22:32 +0900

    Add particle system

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -3,7 +3,10 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     [SerializeField] Player player;
+    [SerializeField] ParticleSystem destroyParticleSystem;
+
     [SerializeField] float moveSpeed;
+    [SerializeField] float attackRange;
 
     void Start()
     {
@@ -18,7 +21,44 @@ public class EnemyController : MonoBehaviour
 
     void Update()
     {
+        FollowPlayer();
+        TryAttack();
+    }
+
+    void FollowPlayer()
+    {
         Vector2 direction = (player.transform.position - transform.position).normalized;
         transform.position = (Vector2) transform.position + direction * moveSpeed * Time.deltaTime;
+    }
+
+    void TryAttack()
+    {
+        // Check the player is in a range of attack
+        float distToPlayer = Vector2.Distance(transform.position, player.transform.position);
+
+        // If the player is in the range, then attack him
+        if (distToPlayer <= attackRange)
+        {
+            // temp
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        // Unparent particle system from game object
+        destroyParticleSystem.transform.parent = null;
+
+        // Destroy game object
+        Destroy(gameObject);
+
+        // Play particle system
+        destroyParticleSystem.Play();
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
     }
 }
